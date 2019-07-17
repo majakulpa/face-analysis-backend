f7d36c2eec8df8612692d48951527083b8aa5dc2
majakulpa <majaweronikakulpa@gmail.com>
2019-07-17 13:38:04 +1000

    Upgrade database

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "majakulpa",
-    password: "",
-    database: "face-reco"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
